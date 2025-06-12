161a3e1140824b0e788503391a8c0a79a5d147cc
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-06-12 22:27:12 +0500

    Main file for running the app

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import View.MainFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame("POS-System Login");
+            }
+        });
     }
 }
