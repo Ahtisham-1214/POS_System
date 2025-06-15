e7aa33a1d6ca03a0bb6f0080941c0d3dd77faefa
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-06-15 14:53:47 +0500

    For handling user operations

---
+++ src/Model/UserDatabase.java
@@ -0,0 +1,35 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDatabase {
+    private final Connection connection;
+
+    public UserDatabase() throws SQLException {
+        connection = DatabaseConnection.getConnection();
+    }
+
+    public String authenticateUser(String userName, String password)
+            throws Exception {
+
+        String query = "SELECT role FROM user WHERE name = ? AND password = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, userName);
+            preparedStatement.setString(2, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    String role = resultSet.getString("role");
+                    System.out.println("Authentication successful. Role: " + role);
+                    return role; // Return the user's role
+                }
+            }
+        }
+        System.out.println("Invalid Credentials");
+        return null; // Return false if authentication fails
+    }
+
+}
