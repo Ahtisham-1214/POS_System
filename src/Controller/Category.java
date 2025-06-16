5d347ca5f414942cc8e8863fc4fc60470c4e5a03
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-06-16 12:41:36 +0500

    For handling categories

--- src/Controller/Category.java
+++ src/Controller/Category.java
@@ -1,4 +1,33 @@
 package Controller;
 
+import java.util.ArrayList;
+
 public class Category {
+    public static ArrayList<Category> categories = new ArrayList<Category>();
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Product Name cannot be null or empty");
+        }
+        this.name = name;
+    }
 }
